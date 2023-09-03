1b24fc1c4f1e2ccb5b7cdfa8e6d14e6d0633bafd
jgb1123 <rmsqja2@gmail.com>
2023-09-03 22:52:41 +0900

    [feat] ToDoList Table로 변경 #6

--- frontend/src/components/ToDoList.vue
+++ frontend/src/components/ToDoList.vue
@@ -1,27 +1,78 @@
-<template>
+<!-- <template>
   <div id="todolist">
-    <q-card v-for= "(item, i) in items" :key="i" class="q-mt-md my-card bg-primary text-white shadow-2">
-      <q-card-section>
-        <div class="text-h5">{{ dayjs(item.targetTime).format('YYYY-MM-DD HH:mm') }}</div>
-        <q-separator dark/>
-        <div class="text-h6">{{ item.title }}</div>
-        <q-separator dark/>
-        <div class="q-ma-md">
-          <q-scroll-area style="height: 50px; max-width: 400px; word-break: break-all">
-            <div class="q-py-xs">{{ item.content }}</div>
-          </q-scroll-area>
-        </div>
+    <q-card>
+      <q-card-section horizontal style="width: 840px">
+        <q-separator vertical />
+        <q-card-section style="width: 200px;"  class="bg-white">
+          <div class="text-h5"> Date </div>
+        </q-card-section>
+        <q-separator vertical />
+        <q-card-section style="width: 250px;" class="bg-white">
+          <div class="text-h5"> Title </div>
+        </q-card-section>
+        <q-separator vertical />
+        <q-card-section style="width: 400px;" class="bg-white">
+          <div class="text-h5"> Content </div>
+        </q-card-section>
+        <q-separator vertical />
+        <q-card-section style="width: 200px;" class="bg-white">
+          <div class="text-h5"> Status</div>
+        </q-card-section>
       </q-card-section>
+    </q-card>
 
-      <q-separator dark/>
-
-      <q-card-actions align="right">
-        <q-btn icon="mode_edit" color="primary" @click="editItem(item)" />
-        <q-btn icon="delete" color="primary" @click="confirm(item.itemId)" />
-      </q-card-actions>
+    <q-card v-for= "(item, i) in items" :key="i" class="q-mt-md bg-white text-black" style="width: 900px;">
+      <q-card-section horizontal>
+        <q-card-section style="width: 200px;"  class="bg-light-blue-4">
+          <div class="text-h5">{{ dayjs(item.targetTime).format('YYYY-MM-DD HH:mm') }}</div>
+        </q-card-section>
+        <q-separator vertical />
+        <q-card-section style="width: 250px;" class="bg-light-blue-3">
+          <div class="text-h6">{{ item.title }}</div>
+        </q-card-section>
+        <q-separator vertical />
+        <q-card-section style="width: 400px;" class="bg-light-blue-2">
+          <div>
+            <q-scroll-area style="height: 50px; max-width: 400px; word-break: break-all">
+              <div class="q-py-xs">{{ item.content }}</div>
+            </q-scroll-area>
+          </div>
+        </q-card-section>
+        <q-separator vertical />
+        <q-card-section style="width: 200px;" class="bg-light-blue-1">
+          <div class="text-h6">{{ item.statusName }}</div>
+        </q-card-section>
+        <q-separator vertical />
+        <q-card-actions align="right" style="width: 60px;">
+          <q-btn icon="mode_edit" color="primary" @click="editItem(item)" />
+          <q-btn icon="delete" color="primary" @click="confirm(item.itemId)" />
+        </q-card-actions>
+      </q-card-section>
     </q-card>
   </div>
+</template> -->
+<template>
+  <div class="q-pa-md">
+    <q-table
+      class="my-sticky-header-table"
+      flat bordered
+      :rows="items"
+      :columns="columns"
+      row-key="name"
+      :hide-pagination="true"
+      :pagination.sync="pagination"
+    >
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          <q-btn icon="mode_edit" color="primary" @click="editItem(props.row)" />
+          <q-btn icon="delete" color="primary" @click="confirm(props.row.itemId)" />
+        </q-td>
+      </template>
+    </q-table>
+  </div>
 </template>
+
+
 
 <script setup>
 import axios from '../utils/axios.js';
@@ -33,6 +84,54 @@ const $q = useQuasar();
 const records = ref(0);
 const items = ref([]);
 const emit = defineEmits(['change-edit-pop-up'])
+
+const pagination = {
+  page: 1,
+  rowsPerPage: 0
+}
+const columns = [
+  {
+    name: 'date',
+    required: true,
+    label: 'Date',
+    align: 'left',
+    field: 'targetTime',
+    format: val => `${dayjs(val).format('YYYY-MM-DD HH:mm')}`,
+    sortable: true
+  },
+  {
+    name: 'title',
+    required: true,
+    label: 'title',
+    align: 'left',
+    field: 'title',
+    format: val => `${val}`,
+    sortable: false
+  },
+  {
+    name: 'content',
+    required: true,
+    label: 'content',
+    align: 'left',
+    field: 'content',
+    format: val => `${val}`,
+    sortable: false
+  },
+  {
+    name: 'status',
+    required: true,
+    label: 'status',
+    align: 'left',
+    field: 'statusName',
+    format: val => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'actions',
+    align: 'center',
+    label: 'Action'
+  }
+]
 
 const getItem = async () => {
   const res = await axios.get('/item/find')
@@ -78,12 +177,26 @@ defineExpose({getItem})
 onMounted( ()=> {
   getItem();
 })
-
 </script>
 
-<style scoped>
-.q-card {
-  width: 600px;
-  height: 240px;
-}
+<style lang="sass">
+.my-sticky-header-table
+  height: 700px
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th
+    background-color: #B8EBFF
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  thead tr:first-child th
+    top: 0
+
+  &.q-table--loading thead tr:last-child th
+    top: 10px
+
+  tbody
+    scroll-margin-top: 48px
 </style>
