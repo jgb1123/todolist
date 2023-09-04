8889229caed81f75be3d5247956f00e9a60b376f
jgb1123 <rmsqja2@gmail.com>
2023-09-04 15:50:45 +0900

    [feat] Board 창 추가, 회원가입 비밀번호 확인 추가 #6 #9

---
+++ frontend/src/components/Board.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="kanban">
+    <div class="flex justify-center">
+      <div class="min-h-screen flex overflow-x-scroll py-12">
+        <div
+            v-for="(status, i) in statuses"
+            :key="i"
+            class="bg-gray-100 rounded-lg px-3 py-3 column-width rounded mr-4"
+        >
+          <p class="text-gray-700 font-semi-bold font-sans tracking-wide text-sm">{{ status.statusName }}</p>
+          <!--           Draggable component comes from vuedraggable. It provides drag & drop functionality -->
+<!--          <draggable :list=status.items :animation="200" ghost-class="ghost-card" group="items">-->
+            <!--             Each element from here will be draggable and animated. Note :key is very important here to be unique both for draggable and animations to be smooth & consistent. -->
+            <task-card
+                v-for="(item, i) in status.items"
+                :key="i"
+                :item="item"
+                class="mt-3 cursor-move"
+            ></task-card>
+            <!--             </transition-group> -->
+<!--          </draggable>-->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import axios from "../utils/axios.js";
+import draggable from "vuedraggable"
+import TaskCard from "./TaskCard.vue";
+
+const statuses = ref([])
+const getStatuses = async () => {
+  const res = await axios.get('status/find/items')
+  if(res.status === 200 ) {
+    statuses.value = res.data.data;
+  }
+}
+
+onMounted(() => {
+  getStatuses()
+})
+
+</script>
+
+<style scoped>
+.column-width {
+  min-width: 320px;
+  width: 320px;
+}
+.ghost-card {
+  opacity: 0.5;
+  background: #F7FAFC;
+  border: 1px solid #4299e1;
+}
+</style>
